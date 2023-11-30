e4983ced722f1a37e90a44b045b05bb5341cffa2
dimavoronkov2222 <dimavoronkov2222@hotmail.com>
2023-11-30 18:17:59 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Microsoft.Data.SqlClient;
+namespace Module_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Data Source=DESKTOP-SE9ORTN;Initial Catalog=Module_1;Integrated Security=True;Trust Server Certificate=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Connection successful!", "Information",MessageBoxButton.OK,MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+    }
+}
